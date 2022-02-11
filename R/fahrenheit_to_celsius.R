df2cf45db1dfa73e0a3cd1e2b4bb1c0bb4a42224
mattclark60 <matthew.clark@ou.edu>
2022-02-11 11:50:47 -0600

    new

---
+++ R/fahrenheit_to_celsius.R
@@ -0,0 +1,15 @@
+#' @title Converts Fahrenheit to Celsius
+#'
+#' @param temp_F The temperature in Fahrenheit
+#'
+#' @return The temperature in Celsius.
+#' @export
+#'
+#' @examples
+#' fahrenheit_to_celsius(32)
+fahrenheit_to_celsius <- function(temp_F) {
+  temp_C <- (temp_F - 32) * 5 / 9
+
+  return(temp_C)
+}
+
